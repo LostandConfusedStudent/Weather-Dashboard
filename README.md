bdd6b086911853fb23293c1927b71346985153a0
pausun <pfsun9999@msn.com>
2020-10-18 14:43:48 -0800

    Updates to readme, HTML, CSS, and JS

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Weather-Dashboard
+# <div align="center">Weather-Dashboard</div>
 
-This is the homework for week six. The objective is to build a weather application that retrieves data from an external API to view the weather of a city of interest.
+## <div align="center">Description</div>
+
+This is the homework for week six. The objective is to build a weather application that retrieves data from an external API to view the weather of a city of interest. After typing entering a city as input and clicking on the submit button, the HTML and CSS code will be updated and the application will display current and future weather conditions for the city of interest. Displayed information will include: city name, date, icon representation, temperature, humidity, wind speed, and UV index. There will also be a search history displayed under the search bar and can be cleared with the clear button.
+
+## <div align="center">Screenshots</div>
+
+## <div align="center">Links</div>
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Define global variables
+
 var yourCity = [];
 
 var key = "c0bfbe029f5184838d438a684303b5b2";
@@ -12,11 +13,11 @@ function citySearch() {
 
     for (var i = 0; i < yourCity.length; i++) {
 
-        var a = $("<li>");
-        a.addClass("city");
-        a.attr("yourCityInput", yourCity[i]);
-        a.text(yourCity[i]);
-        $("#cityView").append(a);
+        var searchHistory = $("<button>");
+        searchHistory.addClass("newCityButton");
+        searchHistory.attr("yourCityInput", yourCity[i]);
+        searchHistory.text(yourCity[i]);
+        $("#cityView").append(searchHistory);
     }
 }
 
@@ -27,7 +28,7 @@ $("#searchButton").on("click", function (event) {
     yourCity.push(cityInput);
 
 
-
+    // url for current weather
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + units + key;
 
     $.ajax({
@@ -36,12 +37,14 @@ $("#searchButton").on("click", function (event) {
     }).then(function (response) {
         // Print weather information
         console.log(response);
+        console.log(response.weather[0].icon);
         console.log(response.weather[0].main);
         console.log(response.main.temp + "\u00B0 Fahrenheit");
         console.log(response.main.humidity + "%");
         console.log(response.wind.speed + " miles per hour");
+        console.log(response.weather[0].description);
 
-
+        // url for uv information
         var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
 
         $.ajax({
@@ -53,6 +56,7 @@ $("#searchButton").on("click", function (event) {
             console.log("UV index: " + response.value);
         });
 
+        // url for forecast for 5 days
         var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + key;
 
         $.ajax({
@@ -80,8 +84,7 @@ $("#searchButton").on("click", function (event) {
 
     // Button to erase input from local storage
     $("#clearHistory").on("click", function () {
-        localStorage.clear();
-        citySearch();
         console.log("Clear button pressed");
     });
 });
+
